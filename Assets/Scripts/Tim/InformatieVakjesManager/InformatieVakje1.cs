858f07ac3664ac26b659c6f28cb65a75e6dd7fbf
Tim Hoevenaar <tjwr.hoevenaar@student.avans.nl>
2025-03-25 13:45:59 +0100

    functionaliteit toegevoegd
    eerste vakje is nu functioneel en heb de naam van het eiland bovenaan gezet

---
+++ Assets/Scripts/Tim/InformatieVakjesManager/InformatieVakje1.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InformatieVakjesManager : MonoBehaviour
+{
+    // alle vakjes
+    public GameObject Vakje1;
+
+    // alle vakjes die je hebt in dit eiland
+    public Button ButtonVakje1;
+
+    // de buttons die in de vakjes zitten
+    public Button ExitVakje1;
+    public Button VerderVakje1;
+    public Button TerugVakje1;
+    public Button AfrondenVakje1;
+
+    // de verschillende soorten informatie per vakje
+    public RawImage TextInformatieVakje1;
+    public RawImage VideoInformatieVakje1;
+
+    void Start()
+    {
+        // Voeg event listeners toe aan de knoppen
+        ExitVakje1.onClick.AddListener(CloseVakje1);
+        VerderVakje1.onClick.AddListener(ShowVideoInformatieVakje1);
+        TerugVakje1.onClick.AddListener(ShowTextInformatieVakje1);
+        AfrondenVakje1.onClick.AddListener(CloseVakje1);
+        ButtonVakje1.onClick.AddListener(OpenVakje1);
+
+        // Initialiseer de vakjes en informatie
+        ResetVakjes();
+    }
+
+    void ResetVakjes()
+    {
+        Vakje1.SetActive(false);
+        TextInformatieVakje1.gameObject.SetActive(false);
+        VideoInformatieVakje1.gameObject.SetActive(false);
+    }
+
+    void OpenVakje1()
+    {
+        Vakje1.SetActive(true);
+        TextInformatieVakje1.gameObject.SetActive(true);
+        VideoInformatieVakje1.gameObject.SetActive(false);
+    }
+
+    void CloseVakje1()
+    {
+        Vakje1.SetActive(false);
+        TextInformatieVakje1.gameObject.SetActive(false);
+        VideoInformatieVakje1.gameObject.SetActive(false);
+    }
+
+
+    void ShowVideoInformatieVakje1()
+    {
+        TextInformatieVakje1.gameObject.SetActive(false);
+        VideoInformatieVakje1.gameObject.SetActive(true);
+    }
+
+    void ShowTextInformatieVakje1()
+    {
+        TextInformatieVakje1.gameObject.SetActive(true);
+        VideoInformatieVakje1.gameObject.SetActive(false);
+    }
+}
+
+
