26e8f9cb579559d83ec08977bda3055a11f5dc6e
Tim Hoevenaar <tjwr.hoevenaar@student.avans.nl>
2025-04-01 21:32:31 +0200

    scene 1 goed gezet met scripts
    scene 1 en 5 werken nu en scene 4 animaties goed gezet alleen nog paar kleine bugs

--- Assets/Scripts/Tim/InlogRegistreerManager.cs
+++ Assets/Scripts/Tim/InlogRegistreerManager.cs
@@ -1,223 +1,217 @@
-//using System;
-//using System.Collections.Generic;
-//using TMPro;
-//using UnityEngine;
-//using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
 
-//public class InlogRegistreerManager : MonoBehaviour
-//{
-//    // scenes
-//    public GameObject Scene1;
-//    public GameObject Scene2;
+public class InlogRegistreerManager : MonoBehaviour
+{
+    // scenes
+    public GameObject Scene1;
+    public GameObject Scene2;
 
-//    // Input fields for registration
-//    public TMP_InputField registerEmailInputField;
-//    public TMP_InputField registerPasswordInputField;
+    // Input fields for registration
+    public TMP_InputField registerEmailInputField;
+    public TMP_InputField registerPasswordInputField;
 
-//    // Input fields for login
-//    public TMP_InputField loginEmailInputField;
-//    public TMP_InputField loginPasswordInputField;
+    // Input fields for login
+    public TMP_InputField loginEmailInputField;
+    public TMP_InputField loginPasswordInputField;
 
-//    public Button loginExit;
-//    public Button registerExit;
+    public Button loginExit;
+    public Button registerExit;
 
-//    public Button registerButton;
-//    public Button loginButton;
+    public Button registerButton;
+    public Button loginButton;
 
-//    public Button gaTerug;
-//    public Button gaDoorZonderAccount;
-//    public Toggle showPasswordToggleInlog; // Toggle om het wachtwoord te verbergen of weer te geven
-//    public Toggle showPasswordToggleRegister;
-//    //public UserApiClient userApiClient;
+    public Button gaTerug;
+    public Button gaDoorZonderAccount;
+    public Toggle showPasswordToggleInlog; // Toggle om het wachtwoord te verbergen of weer te geven
+    public Toggle showPasswordToggleRegister;
+    //public UserApiClient userApiClient;
 
-//    // Buttons to switch between login and register screens
-//    public Button switchToLoginButton;
-//    public Button switchToRegisterButton;
-//    public Button StartGame;
+    // Buttons to switch between login and register screens
+    public Button switchToLoginButton;
+    public Button switchToRegisterButton;
+    public Button StartGame;
 
-//    // Panels for login and registration
-//    public GameObject loginPanel;
-//    public GameObject registerPanel;
-//    public GameObject MainMenuButtons;
-//    public GameObject NotLoggedInWarning;
+    // Panels for login and registration
+    public GameObject loginPanel;
+    public GameObject registerPanel;
+    public GameObject MainMenuButtons;
+    public GameObject NotLoggedInWarning;
 
-//    // Warning components
-//    public RawImage passwordWarningImage;
-//    public TextMeshProUGUI passwordWarningText;
+    // Warning components
+    public RawImage passwordWarningImage;
+    public TextMeshProUGUI passwordWarningText;
 
-//    public List<User> Users = new List<User>();
+    public List<User> Users = new List<User>();
 
-//    private bool isLoggedIn = false;
+    private bool isLoggedIn = false;
 
-//    private void Start()
-//    {
-//        registerButton.onClick.AddListener(Register);
-//        loginButton.onClick.AddListener(Login);
-//        showPasswordToggleInlog.onValueChanged.AddListener(TogglePasswordVisibilityInlog);
-//        showPasswordToggleRegister.onValueChanged.AddListener(TogglePasswordVisibilityRegister);
-//        loginPasswordInputField.contentType = TMP_InputField.ContentType.Password; // Standaard wachtwoord verbergen
-//        registerPasswordInputField.contentType = TMP_InputField.ContentType.Password; // Standaard wachtwoord verbergen
+    private void Start()
+    {
+        registerButton.onClick.AddListener(Register);
+        loginButton.onClick.AddListener(Login);
+        showPasswordToggleInlog.onValueChanged.AddListener(TogglePasswordVisibilityInlog);
+        showPasswordToggleRegister.onValueChanged.AddListener(TogglePasswordVisibilityRegister);
+        loginPasswordInputField.contentType = TMP_InputField.ContentType.Password; // Standaard wachtwoord verbergen
+        registerPasswordInputField.contentType = TMP_InputField.ContentType.Password; // Standaard wachtwoord verbergen
 
-//        switchToLoginButton.onClick.AddListener(ShowLoginPanel);
-//        switchToRegisterButton.onClick.AddListener(ShowRegisterPanel);
+        switchToLoginButton.onClick.AddListener(ShowLoginPanel);
+        switchToRegisterButton.onClick.AddListener(ShowRegisterPanel);
 
-//        loginExit.onClick.AddListener(HideLoginPanel);
-//        registerExit.onClick.AddListener(HideRegisterPanel);
+        loginExit.onClick.AddListener(HideLoginPanel);
+        registerExit.onClick.AddListener(HideRegisterPanel);
 
-//        StartGame.onClick.AddListener(StartGameHandler);
-//        gaTerug.onClick.AddListener(HideNotLoggedInWarning);
-//        gaDoorZonderAccount.onClick.AddListener(ProceedWithoutAccount);
+        StartGame.onClick.AddListener(StartGameHandler);
+        gaTerug.onClick.AddListener(HideNotLoggedInWarning);
+        gaDoorZonderAccount.onClick.AddListener(ProceedWithoutAccount);
 
-//        registerPasswordInputField.onValueChanged.AddListener(ValidateRegisterPassword);
-//        ValidateRegisterPassword(registerPasswordInputField.text); // Initial validation
+        registerPasswordInputField.onValueChanged.AddListener(ValidateRegisterPassword);
+        ValidateRegisterPassword(registerPasswordInputField.text); // Initial validation
 
-//        // Hide warning components initially
-//        passwordWarningImage.gameObject.SetActive(false);
-//        passwordWarningText.gameObject.SetActive(false);
-//    }
+        // Hide warning components initially
+        passwordWarningImage.gameObject.SetActive(false);
+        passwordWarningText.gameObject.SetActive(false);
+    }
 
-//    private void Register()
-//    {
-//        string email = registerEmailInputField.text;
-//        string password = registerPasswordInputField.text;
+    private void Register()
+    {
+        string email = registerEmailInputField.text;
+        string password = registerPasswordInputField.text;
 
-//        if (password.Length < 1 || password.Length > 16)
-//        {
-//            Debug.Log("Password must be between 1 and 16 characters. Registration failed.");
-//            passwordWarningImage.gameObject.SetActive(true);
-//            passwordWarningText.gameObject.SetActive(true);
-//            return;
-//        }
+        if (password.Length < 1 || password.Length > 16)
+        {
+            Debug.Log("Password must be between 1 and 16 characters. Registration failed.");
+            passwordWarningImage.gameObject.SetActive(true);
+            passwordWarningText.gameObject.SetActive(true);
+            return;
+        }
 
-//        User user = new User
-//        {
-//            email = email,
-//            password = password
-//        };
+        User user = new User
+        {
+            email = email,
+            password = password
+        };
 
-//        Users.Add(user);
-//        Debug.Log("User registered: " + email);
-//    }
+        Users.Add(user);
+        Debug.Log("User registered: " + email);
+    }
 
-//    private void Login()
-//    {
-//        string email = loginEmailInputField.text;
-//        string password = loginPasswordInputField.text;
+    private void Login()
+    {
+        string email = loginEmailInputField.text;
+        string password = loginPasswordInputField.text;
 
-//        User user = Users.Find(u => u.email == email && u.password == password);
+        User user = Users.Find(u => u.email == email && u.password == password);
 
-//        if (user != null)
-//        {
-//            isLoggedIn = true;
-//            Debug.Log("Login success!");
-//        }
-//        else
-//        {
-//            Debug.Log("Login failed: Invalid email or password");
-//        }
-//    }
+        if (user != null)
+        {
+            isLoggedIn = true;
+            Debug.Log("Login success!");
+        }
+        else
+        {
+            Debug.Log("Login failed: Invalid email or password");
+        }
+    }
 
-//    private void ShowLoginPanel()
-//    {
-//        Debug.Log("Showing login panel");
-//        loginPanel.SetActive(true);
-//        registerPanel.SetActive(false);
-//        MainMenuButtons.SetActive(false);
-//    }
+    private void ShowLoginPanel()
+    {
+        Debug.Log("Showing login panel");
+        loginPanel.SetActive(true);
+        registerPanel.SetActive(false);
+        MainMenuButtons.SetActive(false);
+    }
 
-//    private void ShowRegisterPanel()
-//    {
-//        Debug.Log("Showing register panel");
-//        loginPanel.SetActive(false);
-//        registerPanel.SetActive(true);
-//        MainMenuButtons.SetActive(false);
-//    }
+    private void ShowRegisterPanel()
+    {
+        Debug.Log("Showing register panel");
+        loginPanel.SetActive(false);
+        registerPanel.SetActive(true);
+        MainMenuButtons.SetActive(false);
+    }
 
-//    private void HideLoginPanel()
-//    {
-//        Debug.Log("Hiding login panel");
-//        loginPanel.SetActive(false);
-//        MainMenuButtons.SetActive(true);
-//    }
+    private void HideLoginPanel()
+    {
+        Debug.Log("Hiding login panel");
+        loginPanel.SetActive(false);
+        MainMenuButtons.SetActive(true);
+    }
 
-//    private void HideRegisterPanel()
-//    {
-//        Debug.Log("Hiding register panel");
-//        registerPanel.SetActive(false);
-//        MainMenuButtons.SetActive(true);
-//    }
+    private void HideRegisterPanel()
+    {
+        Debug.Log("Hiding register panel");
+        registerPanel.SetActive(false);
+        MainMenuButtons.SetActive(true);
+    }
 
-//    private void ShowNotLoggedInWarning()
-//    {
-//        Debug.Log("Showing not logged in warning");
-//        NotLoggedInWarning.SetActive(true);
-//    }
+    private void ShowNotLoggedInWarning()
+    {
+        Debug.Log("Showing not logged in warning");
+        NotLoggedInWarning.SetActive(true);
+    }
 
-//    private void HideNotLoggedInWarning()
-//    {
-//        Debug.Log("Hiding not logged in warning");
-//        NotLoggedInWarning.SetActive(false);
-//    }
+    private void HideNotLoggedInWarning()
+    {
+        Debug.Log("Hiding not logged in warning");
+        NotLoggedInWarning.SetActive(false);
+    }
 
-//    private void ProceedWithoutAccount()
-//    {
-//        Debug.Log("Proceeding without account");
-//        Scene1.SetActive(false);
-//        Scene2.SetActive(true);
-//    }
+    private void ProceedWithoutAccount()
+    {
+        Debug.Log("Proceeding without account");
+        Scene1.SetActive(false);
+        Scene2.SetActive(true);
+    }
 
-//    private void ProceedWithAccount()
-//    {
-//        Debug.Log("Proceeding with account");
-//        Scene1.SetActive(false);
-//        Scene2.SetActive(true);
-//    }
+    private void ProceedWithAccount()
+    {
+        Debug.Log("Proceeding with account");
+        Scene1.SetActive(false);
+        Scene2.SetActive(true);
+    }
 
-//    private void StartGameHandler()
-//    {
-//        if (isLoggedIn)
-//        {
-//            ProceedWithAccount();
-//        }
-//        else
-//        {
-//            ShowNotLoggedInWarning();
-//        }
-//    }
+    private void StartGameHandler()
+    {
+        if (isLoggedIn)
+        {
+            ProceedWithAccount();
+        }
+        else
+        {
+            ShowNotLoggedInWarning();
+        }
+    }
 
-//    private void TogglePasswordVisibilityInlog(bool isVisible)
-//    {
-//        loginPasswordInputField.contentType = isVisible ? TMP_InputField.ContentType.Standard : TMP_InputField.ContentType.Password;
-//        loginPasswordInputField.ForceLabelUpdate(); // Forceer een update om de wijziging door te voeren
-//    }
+    private void TogglePasswordVisibilityInlog(bool isVisible)
+    {
+        loginPasswordInputField.contentType = isVisible ? TMP_InputField.ContentType.Standard : TMP_InputField.ContentType.Password;
+        loginPasswordInputField.ForceLabelUpdate(); // Forceer een update om de wijziging door te voeren
+    }
 
-//    private void TogglePasswordVisibilityRegister(bool isVisible)
-//    {
-//        registerPasswordInputField.contentType = isVisible ? TMP_InputField.ContentType.Standard : TMP_InputField.ContentType.Password;
-//        registerPasswordInputField.ForceLabelUpdate(); // Forceer een update om de wijziging door te voeren
-//    }
+    private void TogglePasswordVisibilityRegister(bool isVisible)
+    {
+        registerPasswordInputField.contentType = isVisible ? TMP_InputField.ContentType.Standard : TMP_InputField.ContentType.Password;
+        registerPasswordInputField.ForceLabelUpdate(); // Forceer een update om de wijziging door te voeren
+    }
 
-//    private void ValidateRegisterPassword(string password)
-//    {
-//        if (password.Length < 1 || password.Length > 16)
-//        {
-//            passwordWarningImage.gameObject.SetActive(true);
-//            passwordWarningText.gameObject.SetActive(true);
-//        }
-//        else
-//        {
-//            passwordWarningImage.gameObject.SetActive(false);
-//            passwordWarningText.gameObject.SetActive(false);
-//        }
-//    }
-//}
+    private void ValidateRegisterPassword(string password)
+    {
+        if (password.Length < 1 || password.Length > 16)
+        {
+            passwordWarningImage.gameObject.SetActive(true);
+            passwordWarningText.gameObject.SetActive(true);
+        }
+        else
+        {
+            passwordWarningImage.gameObject.SetActive(false);
+            passwordWarningText.gameObject.SetActive(false);
+        }
+    }
+}
 
-//[Serializable]
-//public class User
-//{
-//    public string email;
-//    public string password;
-//}
 
 
 
